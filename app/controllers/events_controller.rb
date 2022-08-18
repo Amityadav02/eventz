072f0147cbdf4d70fc2afc17a6a1be1f0848bcf1
Amit yadav <Amit.yadav@gemsessence.com>
2022-08-18 18:11:24 +0530

    indent fix

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,8 +1,8 @@
 class EventsController < ApplicationController
 
   def index
-	 @events = Event.upcoming 
-	end
+    @events = Event.upcoming 
+  end
 
 
 	def show
@@ -14,40 +14,40 @@ class EventsController < ApplicationController
 	end
 
 	def update
-		@event = Event.find(params[:id])
-			@event.update(event_params)
-			redirect_to @event
-	 end
+	  @event = Event.find(params[:id])
+	  @event.update(event_params)
+	  redirect_to @event
+	end
 
-	 def new
-		 @event = Event.new
-	 end
+  def new
+    @event = Event.new
+  end
 
-	 def create 
-		@event= Event.new(event_params)
-		@event.save
-		 redirect_to @event
-	 end
+  def create
+    @event= Event.new(event_params)
+    @event.save
+    redirect_to @event
+  end
 
-	 def destroy
+  def destroy
 		@event = Event.find(params[:id])
 		@event.destroy
 		redirect_to events_url
-	 end
+  end
 
 
-	 private
+  private
 
-	   def event_params  
-			params.require(:event).
-		      permit(:name, :description, :location, :price, :starts_at,
-		             :capacity, :image_file_name)
-		 end
-		end
+    def event_params  
+      params.require(:event).
+        permit(:name, :description, :location, :price, :starts_at,
+             :capacity, :image_file_name)
+    end
+end
 
 
 
-		 
+ 
 	 
 
 
