f53e031e7f8247eea01954f761fe9c8211a7ef77
Amit yadav <Amit.yadav@gemsessence.com>
2022-08-19 15:39:09 +0530

    22 has many

--- app/models/registration.rb
+++ app/models/registration.rb
@@ -1,3 +1,19 @@
 class Registration < ApplicationRecord
   belongs_to :event
+
+  validates :name, presence: true
+
+  validates :email, format: { with: /\s+@\s+/ }
+
+  HOW_HEARD_OPTIONS = [
+    'News latter',
+    'Blog post',
+    'Twitter',
+    'Web search',
+    'Friend/Coworker',
+    'other'
+  ]
+
+  validates :how_heard,
+            inclusion: { in: how_heard_options }
 end
