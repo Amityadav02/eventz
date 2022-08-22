2a7344b309a078bb0f691fb1bb6e1dbe609c6cac
Amit yadav <Amit.yadav@gemsessence.com>
2022-08-22 19:00:33 +0530

    added 33 authorization

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,9 @@ class SessionsController < ApplicationController
 			user = User.find_by(email: params[:email])
       if user && user.authenticate(params[:password])
         session[:user_id] = user.id
-        redirect_to user, notice: "Welcome back, #{user.name}!"
+        redirect_to (session[:intendened_url] || user), 
+                    notice: "Welcome back, #{user.name}!"
+        session[:intendened_url] = nil            
       else
         flash.now[:alert] = "Invalid email/password combination!"
         render :new
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
 
+	before_action :require_signin, except: [:new, :create]
+	before_action :require_correct_user, only: [:edit, :update, :destroy]
+
 	def index
 		@users = User.all
   end
@@ -23,11 +26,9 @@ class UsersController < ApplicationController
 	end
 
 	def edit
-		@user = User.find(params[:id])
 	end
 
 	def update
-	  @user = User.find(params[:id])
 	  if @user.update(user_params)
 		   redirect_to @user, notice: "Account succesfully updated!"
 		 else
@@ -36,19 +37,23 @@ class UsersController < ApplicationController
 	end
 
 	def destroy
-		@user = User.find(params[:id])
 		@user.destroy
 		redirect_to events_url, alert: "Account succesfully deleted"
 		session[:user_id] = nil
 	end
 
 
-	private
+private
+
+  def require_correct_user
+	@user = User.find(params[:id])
+	  redirect_to events_url unless current_user?(@user)
+  end
 	
-	  def user_params
-		params.require(:user).
-		  permit(:name, :email, :password, :password_confirmation)
-	  end
+  def user_params
+	params.require(:user).
+	 permit(:name, :email, :password, :password_confirmation)
+  end
 	
 	
 	
